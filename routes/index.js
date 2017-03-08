913def2ba0c21245acc360a55c692149edd2c235
vaibhavpathak1994 <vaibhav_pathak@excellencetechnologies.in>
2017-03-08 19:21:35 +0530

    frist scrap category the fetch mobile data

--- routes/index.js
+++ routes/index.js
@@ -5,8 +5,9 @@ var zlib = require('zlib');
 var async = require('async');
 var cheerio = require('cheerio');
 
+<!----- fetch T-Shirt data -------->
+
 router.all('/fetch/snapdeal/mobile', function(req, res, next) {
-    urls = [];
     record1 = [];
     var url = ['https://www.snapdeal.com/products/mens-tshirts-polos?sort=plrty'];
     var fetch = req.fetch;
@@ -39,7 +40,6 @@ router.all('/fetch/snapdeal/mobile', function(req, res, next) {
             $('.product-tuple-listing').each(function() {
                 var name = $(this).find('p.product-title').text();
                 var price = $(this).find('.product-desc-price').text();
-                // var src = $(this).find('img.product-image').attr('src');
                 var href = $(this).find("a.noUdLine").attr("href");
 
                 var product = { LINK: href, Name: name, Price: price };
@@ -51,6 +51,81 @@ router.all('/fetch/snapdeal/mobile', function(req, res, next) {
         });
     }
 
+});
+
+
+
+<!-----fetch mobile data---->
+
+router.all('/fetch/snapdeal/mobile/full', function(req, res) {
+    record1 = [];
+    var url = ['https://www.snapdeal.com/'];
+    var categoryUrl;
+    var mobileUrl;
+
+    function scrapHref(url, callback) {
+        request(url, function(err, response, body) {
+            var $ = cheerio.load(body);
+            var href = $('#category2Data').find('.rightMenuLink ').attr('href');
+            categoryUrl = [href];
+
+            async.eachSeries(categoryUrl, scrapmobile, function(err) {
+                callback();
+            })
+        })
+    }
+
+    function scrapmobile(categoryUrl, callback) {
+        request(categoryUrl, function(err, response, detail) {
+            var $ = cheerio.load(detail);
+            var details = [];
+            $('.trending-slider-csf').find('a').each(function() {
+                details.push($(this).attr('href'))
+            });
+            mobileUrl = [details[6]];
+            async.eachSeries(mobileUrl, scrapData, function(err) {
+                callback();
+            })
+        })
+    }
+
+    function scrapData(mobileUrl, callback) {
+        request(mobileUrl, function(err, resp, data) {
+            var $ = cheerio.load(data);
+            res.send(data);
+            // fetching url here
+            $('.product-tuple-image ').each(function() {
+                var href = $(this).find('a').attr("href");
+                var name = $(this).find(".product-image").attr('title');
+                // var price = $(this).find('.product-desc-price').attr('display-price');
+                var src = $(this).find("img.product-image").attr('src');
+                var product = { LINK: href, NAME: name, SRC: src };
+                record1.push(product);
+            });
+            async.eachSeries(record1, saveProduct, function(err, result) {
+                callback()
+            })
+        });
+    }
+
+    function saveProduct(product, callback) {
+        var record = new req.fetch({
+            "LINK": product.LINK,
+            "Name": product.NAME,
+            "Src": product.SRC,
+        });
+        record.save(function(err, data) {
+            if (err) {
+                throw err;
+                callback();
+            } else {
+                callback();
+            }
+        });
+    }
+    async.eachSeries(url, scrapHref, function(err, result) {
+
+    })
 });
 
 
